b6b7b9f0c8df584aa27edf8f8e8b3f64bf158349
Parth-bathe0142 <parth.bathe0142@gmail.com>
2026-03-09 20:28:38 +0530

    added an option to convert all files in a directory

--- wat-printer/src/lib.rs
+++ wat-printer/src/lib.rs
@@ -1,15 +1,59 @@
-use std::{error::Error, path::Path};
+use anyhow::{Error, Result};
+use std::{fs, path::Path};
 
-pub fn print(input: &str, output: &str) -> Result<(), Box<dyn Error>> {
-	let path = Path::new(input);
-	
-    if !path.try_exists()? {
-        return Err(format!("input file not found: {}", input).into());
+pub fn print(input: &Path, output: &Path) -> Result<()> {
+    if !input.try_exists()? {
+        return Err(Error::msg("Could not find the specified file"));
+    }
+
+    let wat = wasmprinter::print_file(input)?;
+
+    std::fs::write(output, wat)?;
+
+    Ok(())
+}
+
+fn print_if_wasm(path: &Path) -> Result<()> {
+    if path.extension().unwrap_or_default() != "wasm" {
+        return Ok(());
     }
 
     let wat = wasmprinter::print_file(path)?;
+    let output = path.with_extension("wat");
+    std::fs::write(&output, wat)?;
+    Ok(())
+}
 
-    std::fs::write(output, wat)?;
-    
+pub fn print_all(path: &Path, recursive: bool) -> Result<()> {
+    if recursive {
+        visit_dirs(path, &print_if_wasm)?;
+    } else {
+        if path.is_dir() {
+            for entry in fs::read_dir(path)? {
+                let entry = entry?;
+                let path = entry.path();
+                if path.is_file() {
+                    print_if_wasm(&path)?;
+                }
+            }
+        } else {
+            print_if_wasm(path)?;
+        }
+    }
+    Ok(())
+}
+
+fn visit_dirs(path: &Path, cb: &impl Fn(&Path) -> Result<()>) -> Result<()> {
+    if path.is_dir() {
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&path)?;
+            }
+        }
+    }
     Ok(())
 }
--- wat-printer/src/main.rs
+++ wat-printer/src/main.rs
@@ -1,18 +1,14 @@
-use std::error::Error;
+use anyhow::{Result, anyhow};
+use std::path::Path;
 
-use clap::{Arg, Command};
+use clap::{Arg, ArgAction, Command};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     let matches = Command::new("Wat Printer")
         .version("1.0")
         .author("Parth Bathe <parth.bathe0142@gmail.com>")
         .about("Converts .wasm files to .wat files")
-        .arg(
-            Arg::new("input")
-                .index(1)
-                .help("The .wasm file to convert")
-                .required(true),
-        )
+        .arg(Arg::new("input").index(1).help("The .wasm file to convert"))
         .arg(
             Arg::new("output")
                 .short('o')
@@ -20,20 +16,39 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .help("The output .wat file, defaults to the same name as input")
                 .required(false),
         )
+        .arg(
+            Arg::new("all")
+                .long("all")
+                .short('a')
+                .conflicts_with_all(["input", "output"])
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("recursive")
+                .requires("all")
+                .long("recursive")
+                .short('r')
+                .action(ArgAction::SetTrue),
+        )
         .get_matches();
 
-    let Some(input) = matches.get_one::<String>("input") else {
-        return Err("input not provided".into());
-    };
+    if matches.get_flag("all") {
+        let recursive = matches.get_flag("recursive");
+        wat_printer::print_all(Path::new("."), recursive)?;
+    } else {
+        let Some(input) = matches.get_one::<String>("input") else {
+            return Err(anyhow!("Input not provided"));
+        };
+        let input = Path::new(input);
 
-    let output = matches
-        .get_one::<String>("output")
-        .map(|s| s.to_string())
-        .unwrap_or_else(|| {
-            let path = input.trim_end_matches(".wasm");
-            format!("{path}.wat")
-        });
+        let output = matches
+            .get_one::<String>("output")
+            .map(|s| s.as_str())
+            .map(Path::new)
+            .unwrap_or(input)
+            .with_extension("wat");
 
-    wat_printer::print(input, &output)?;
+        wat_printer::print(input, &output)?;
+    }
     Ok(())
 }
